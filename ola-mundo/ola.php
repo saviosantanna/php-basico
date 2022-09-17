acab450c9ffed74a8e42dd8aa80657f8c5702908
saviosantanna <saviosantannadesa@gmail.com>
2022-09-16 23:26:18 -0300

    Update ola.php

--- ola-mundo/ola.php
+++ ola-mundo/ola.php
@@ -5,11 +5,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Primeiro Exemplo PHP</title>
+    <style>
+        h2 {
+            color: red;
+            text-shadow: 1px 1px 1px black
+        }
+    </style>
 </head>
 <body>
     <h1>Testando</h1>
     <?php
-        echo "Olá, Mundo!";
+        echo "<h2>Olá, <br>Mundo!</h2>";
     ?>
 </body>
 </html>
